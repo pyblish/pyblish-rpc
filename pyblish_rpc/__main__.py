ed7aaa11853bec598c81eb1aa49548fb32f48113
Marcus Ottosson <konstruktion@gmail.com>
2015-06-12 09:40:15 +0100

    Adding support for non-debug mode via CLI

--- pyblish_rpc/__main__.py
+++ pyblish_rpc/__main__.py
@@ -8,7 +8,13 @@ if __name__ == '__main__':
                         help="Port to use")
     parser.add_argument("--delay", type=float, default=0.5,
                         help="Higher value means slower")
+    parser.add_argument("--debug", action="store_true", default=False)
 
     args = parser.parse_args()
 
-    server.start_debug_server(**args.__dict__)
+    if args.debug:
+        print "Starting debug server.."
+        server.start_debug_server(**args.__dict__)
+    else:
+        print "Starting production server.."
+        server.start_production_server(port=args.port)
