5e4741c87e5c74c5bafea2c38bf271d74a0314aa
Marcus Ottosson <konstruktion@gmail.com>
2015-06-12 09:40:02 +0100

    Adding test harness for testing over processes

---
+++ tests/test_production.py
@@ -0,0 +1,46 @@
+"""Assume behaviour on part of the core Pyblish library"""
+
+import sys
+import subprocess
+
+import pyblish_rpc.client
+import pyblish_rpc.server
+
+import pyblish.api
+
+from nose.tools import (
+    with_setup,
+    assert_equals,
+    assert_false,
+    assert_true
+)
+
+server = None
+thread = None
+process = None
+client = None
+
+self = sys.modules[__name__]
+
+
+def setup():
+    """Subprocessed RPC server
+
+    The benefit of a processed server, as opposed to threaded
+    is that it accurately reflects the production server, at
+    the cost of not allowing plug-ins to be registered during
+    a test.
+
+    """
+
+    self.process = subprocess.Popen(
+        ["python", "-m", "pyblish_rpc", "--port", str(7001)])
+    self.client = pyblish_rpc.client.Proxy(7001)
+
+
+def teardown():
+    """Shutdown module-level server"""
+    self.client.kill()
+    print "Killed"
+    self.process.wait()
+    assert not process.poll()
